11655cc3ba2b9ed2e1e5942bbc43e9cea373a776
@hexlet/cli <support@hexlet.io>
2023-05-06 13:39:33 +0300

    submit java-collections-ru/lists_06.05.23_13.36.58

--- java-collections-ru/lists_06.05.23_13.36.58/src/main/java/exercise/App.java
+++ java-collections-ru/lists_06.05.23_13.36.58/src/main/java/exercise/App.java
@@ -14,11 +14,10 @@ class App {
         }
 
         for (int i = 0; i < word.length(); i++) {
-
             if (list.contains(word.substring(i, i + 1).toLowerCase())) {
                 list.remove(word.substring(i, i + 1));
-              } else {
-              return false;
+            } else {
+                return false;
             }
         }
 
