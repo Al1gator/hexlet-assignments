f746a1a50680a78d5bba0ea8193fe9edaff65cb2
@hexlet/cli <support@hexlet.io>
2023-05-06 13:24:54 +0300

    submit java-collections-ru/lists_06.05.23_09.52.05

--- java-collections-ru/lists_06.05.23_09.52.05/src/main/java/exercise/App.java
+++ java-collections-ru/lists_06.05.23_09.52.05/src/main/java/exercise/App.java
@@ -5,22 +5,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
-public static boolean scrabble(String kit, String word) {
-    List<String> list = new ArrayList<>();
+class App {
+    public static boolean scrabble(String kit, String word) {
+        List<String> list = new ArrayList<>();
 
-    for (int i = 0; i < kit.length(); i++) {
-        list.add(kit.substring(i, i + 1));
-    }
-
-    for (int i = 0; i < word.length(); i++) {
-
-        if (list.contains(word.substring(i, i + 1).toLowerCase())) {
-            list.remove(word.substring(i, i + 1));
-          } else {
-          return false;
+        for (int i = 0; i < kit.length(); i++) {
+            list.add(kit.substring(i, i + 1));
         }
-    }
 
-    return true;
+        for (int i = 0; i < word.length(); i++) {
+
+            if (list.contains(word.substring(i, i + 1).toLowerCase())) {
+                list.remove(word.substring(i, i + 1));
+            } else {
+              return false;
+            }
+        }
+
+        return true;
+    }
 }
 //END
